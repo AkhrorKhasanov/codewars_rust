7fb7e37e553da13ffa1974104801ceb61d19567a
AkhrorKhasanov <ahrorhasanov815@gmail.com>
2026-06-21 12:26:38 +0500

    21.06.2026 12:26

---
+++ 8 kyu/Hex to Decimal.rs
@@ -0,0 +1,27 @@
+use std::collections::HashMap;
+fn hex_to_dec(hex_string: &str) -> u32 {
+    let data = HashMap::from([
+        ('0', 0),
+        ('1', 1),
+        ('2', 2),
+        ('3', 3),
+        ('4', 4),
+        ('5', 5),
+        ('6', 6),
+        ('7', 7),
+        ('8', 8),
+        ('9', 9),
+        ('a', 10),
+        ('b', 11),
+        ('c', 12),
+        ('d', 13),
+        ('e', 14),
+        ('f', 15),
+    ]);
+    let mut res: u32 = 0;
+    hex_string.chars().rev().enumerate().map(|(i, c) | {
+        let digit = map.get(&c).unwrap_or(&0);
+
+    }).sum()
+}
+
